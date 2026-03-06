69d2336536ae7a352550cfce11c65a2c80a6c57a
DANIA-DESKTOP\user <ainuldania@gmail.com>
2026-03-07 01:12:38 +0800

    Added appsettings.json config file and create table with dapper

---
+++ DatabaseManager.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
+
+namespace CodingTracker
+{
+    internal class DatabaseManager
+    {
+        //Appsetting.json config connection
+        public string GetConnectionString()
+        {
+            IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            string connectionString = config.GetConnectionString("DefaultConnection");
+
+            return connectionString;
+
+        }
+        
+        internal void CreateTable()
+        {
+            using (var connection = new SqliteConnection(GetConnectionString()))
+            {
+                var sql = @"CREATE TABLE IF NOT EXISTS coding_tracker(
+                            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            StartDate TEXT,
+                            EndDate TEXT,
+                            Duration TEXT)";
+
+                connection.Execute(sql); //Dapper execute
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DatabaseManager databaseManager = new DatabaseManager();
+            databaseManager.CreateTable();
         }
     }
 }
