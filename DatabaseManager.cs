0ff250efffedef9766878bfacc5757edbb7847c7
DANIA-DESKTOP\user <ainuldania@gmail.com>
2026-03-09 18:44:54 +0800

    Added Update method and Delete method

--- DatabaseManager.cs
+++ DatabaseManager.cs
@@ -31,6 +31,7 @@ namespace CodingTracker
             }
         }
 
+        // Return rows affected checks if the execute succeeds or not
         internal int Post(CodingSession session)
         {
             using (var connection = new SqliteConnection(GetConnectionString()))
@@ -52,6 +53,53 @@ namespace CodingTracker
 
                 return sessions.ToList();
             }
+        }
+
+
+        internal void Delete(int id)
+        {
+            using (var connection = new SqliteConnection(GetConnectionString()))
+            {
+                var sql = "DELETE FROM coding_tracker WHERE Id = @Id";
+
+                connection.Execute(sql, new {Id = id });
+            }
+        }
+
+        internal void Update(CodingSession session)
+        {
+            using (var connection = new SqliteConnection(GetConnectionString()))
+            {
+                var sql = @"UPDATE coding_tracker 
+                            SET StartTime = @StartTime, 
+                            EndTime = @EndTime, 
+                            Duration = @Duration
+                            WHERE Id = @Id";
+
+                connection.Execute(sql, session);
+            }
+        }
+
+        internal bool CheckIdExists(int id)
+        {
+            using (var connection = new SqliteConnection(GetConnectionString()))
+            {
+                var sql = "SELECT COUNT(*) FROM coding_tracker WHERE Id = @Id";
+
+                int count = connection.ExecuteScalar<int>(sql, new { Id = id});
+
+                return count > 0;
+            }
+        }
+
+        internal CodingSession GetSessionByID(int id)
+        {
+            using (var connection = new SqliteConnection(GetConnectionString()))
+            {
+                var sql = "SELECT * FROM coding_tracker where Id = @Id";
+                return connection.QuerySingle<CodingSession>(sql, new { Id = id });
+            }
+
         }
     }
 }
--- UserInterface.cs
+++ UserInterface.cs
@@ -48,20 +48,16 @@ namespace CodingTracker
         {
             var sessions = databaseManager.Get();
 
-            var table = new Table();
-
-            table.AddColumn("[bold]ID[/]");
-            table.AddColumn("[bold]Start Date[/]");
-            table.AddColumn("[bold]End Date[/]");
-            table.AddColumn("[bold]Duration[/]");
-
-            foreach (var session in sessions)
+            if (sessions.Count == 0)
             {
-                table.AddRow(session.Id.ToString(),$"{session.StartTime}",$"{session.EndTime}",$"{session.Duration}");
+                AnsiConsole.MarkupLine("[red]No sessions recorded.[/]\nPress enter key to go back to Main Menu.");
+                Console.ReadLine();
+                return;
             }
 
-            AnsiConsole.Write(table);
-            AnsiConsole.MarkupLine("Press enter key to go back to Main Menu");
+            DrawTable(sessions);
+
+            AnsiConsole.MarkupLine("Press enter key to go back to Main Menu.");
             Console.ReadLine();
         }
 
@@ -106,20 +102,138 @@ namespace CodingTracker
                     Console.ReadLine();
                 }
 
-                
             }
             
         }
 
         private void UpdateSessions()
         {
+            AnsiConsole.Clear();
+            AnsiConsole.MarkupLine("[bold yellow]---Update Coding Sessions---[/]");
 
+            var sessions = databaseManager.Get();
+
+            if (sessions.Count == 0)
+            {
+                AnsiConsole.MarkupLine("\n[red]No sessions recorded to update.[/]\nPress enter key to go back to Main Menu.");
+                Console.ReadLine();
+                return;
+            }
+
+            DrawTable(sessions);
+
+            int inputID = AnsiConsole.Ask<int>("Please insert the number ID to update the session (or type 0 to go back to the Main Menu). ");
+
+            if (inputID == 0) return;
+
+            if (!databaseManager.CheckIdExists(inputID))
+            {
+                AnsiConsole.MarkupLine($"\n[red]Session with ID {inputID} doesn't exist.[/]");
+                Console.ReadLine();
+                UpdateSessions();
+            }
+            else
+            {
+                var sessionChoice = databaseManager.GetSessionByID(inputID);
+
+                bool isUpdating = true;
+                while (isUpdating)
+                {
+                    AnsiConsole.Clear();
+                    AnsiConsole.MarkupLine("[bold yellow]---Update Coding Sessions---[/]");
+                    DrawTable(sessions);
+
+                    var choice = AnsiConsole.Prompt(new SelectionPrompt<string>()
+                        .Title($"What would you like to do with Session {inputID}?")
+                        .AddChoices("Update Start Time","Update End Time","Save and Update Database","Return to Main Menu"));
+
+                    switch (choice)
+                    {
+                        case "Update Start Time":
+                            AnsiConsole.MarkupLine("Please insert the [bold green]start[/] date and time");
+                            sessionChoice.StartTime = helpers.CheckDateTime();
+                            break;
+                        case "Update End Time":
+                            AnsiConsole.MarkupLine("\nPlease insert the [bold green]end[/] date and time (or type 0 to go back to the Main Menu).");
+                            sessionChoice.EndTime = helpers.CheckDateTime();
+                            break;
+                        case "Save and Update Database":
+                            var duration = helpers.GetDuration(sessionChoice.StartTime, sessionChoice.EndTime);
+                            if (duration < TimeSpan.Zero)
+                            {
+                                AnsiConsole.Markup("[red]End date cannot be before start date! Please try again.[/]\n");
+                                Console.ReadLine();
+                            }
+                            else
+                            {
+                                sessionChoice.Duration = $"{(int)duration.TotalHours:D2}:{duration.Minutes:D2}";
+                                databaseManager.Update(sessionChoice);
+                                AnsiConsole.Markup("[green]Session has been updated![/]\n");
+                                Console.ReadLine();
+                                isUpdating = false;
+                            }
+                            break;
+                        case "Return to Main Menu":
+                            isUpdating = false;
+                            break;
+
+                    }                
+                }
+
+            }
         }
 
 
         private void DeleteSessions()
         {
-            
+            AnsiConsole.Clear();
+            AnsiConsole.MarkupLine("[bold yellow]---Delete Coding Sessions---[/]");
+
+            var sessions = databaseManager.Get();
+
+            if (sessions.Count == 0)
+            {
+                AnsiConsole.MarkupLine("\n[red]No sessions recorded to delete.[/]\nPress enter key to go back to Main Menu.");
+                Console.ReadLine();
+                return;
+            }
+
+            DrawTable(sessions);
+
+            int inputID = AnsiConsole.Ask<int>("Please insert the number ID to delete the session (or type 0 to go back to the Main Menu). ");
+
+            if (inputID == 0) return;
+
+            if (!databaseManager.CheckIdExists(inputID))
+            {
+                AnsiConsole.MarkupLine($"\n[red]Session with ID {inputID} doesn't exist.[/]");
+                Console.ReadLine();
+                DeleteSessions();
+            }
+            else
+            {
+                databaseManager.Delete(inputID);
+                AnsiConsole.MarkupLine($"\n[green]Session with ID {inputID} has been deleted![/]");
+                Console.ReadLine();
+            }
+        }
+
+
+        private void DrawTable(List<CodingSession> sessions)
+        {
+            var table = new Table();
+
+            table.AddColumn("[bold]ID[/]");
+            table.AddColumn("[bold]Start Date[/]");
+            table.AddColumn("[bold]End Date[/]");
+            table.AddColumn("[bold]Duration[/]");
+
+            foreach (var session in sessions)
+            {
+                table.AddRow(session.Id.ToString(), $"{session.StartTime}", $"{session.EndTime}", $"{session.Duration}");
+            }
+
+            AnsiConsole.Write(table);
         }
         
     }
